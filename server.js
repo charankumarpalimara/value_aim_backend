b763edf6f86caeff28f4b9a95c16a6deccc60bab
Palimara Charan <palimaracharan@Palimaras-MacBook-Air.local>
2025-10-23 22:29:51 +0530

    cors updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173' || 'https://valueaim.com/',
   credentials: true
 }));
 app.use(express.json());
